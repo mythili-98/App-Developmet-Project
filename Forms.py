96a186f2fcd528829e8e938cc3ae501439ca955a
mythili-98 <givemynameback98@gmail.com>
2019-12-13 09:48:28 +0800

    added admin login page

--- Forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, RadioField, validators
-
-class CreateUserForm(Form):
- firstName = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
- lastName = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
- email = StringField("Email", [validators.Length(min=10, max=100), validators.DataRequired()])
- membership = RadioField('Membership', choices=[('M', 'Member'), ('P', 'Premium Member')], default='M')
- password = StringField('Password', [validators.Length(min=5, max=150), validators.DataRequired()])
--- Main.py
+++ Main.py
@@ -1,29 +1,13 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for
-from Forms import CreateUserForm
+from flask import render_template
 
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
-@app.route('/contact')
-def contact():
-    return render_template("contactUs.html")
-@app.route('/login')
-def login():
-    return render_template("login.html")
-@app.route('/products')
-def products():
-    return render_template("products.html")
-@app.route('/registration')
-def registration():
-    return render_template("registration.html")
-@app.route('/search')
-def search():
-    return render_template("search.html")
-@app.route('/brownmouse')
-def brownmouse():
-    return render_template("brownmouse.html")
+@app.route('/admin_login')
+def admin_login():
+    return render_template('admin_login.html')
 
 if __name__ == '__main__':
  app.run()
